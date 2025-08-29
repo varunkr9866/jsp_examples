7a93f050bca5ca767cb34e9c98bcb52e392568e0
varun <varunkr9866@gmail.com>
2025-08-29 14:41:37 +0530

    COMPLETED THE SERVLET

--- 13servletjspunamepassword/src/main/java/com/cluster/CheckServlet.java
+++ 13servletjspunamepassword/src/main/java/com/cluster/CheckServlet.java
@@ -9,5 +9,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class CheckServlet extends HttpServlet{
-	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		System.out.println("Inside CheckServlet**************");
+		String strNme = req.getParameter("nme");
+		String strPwd = req.getParameter("pwd");
+		RequestDispatcher rd = null;
+		if (strNme.equalsIgnoreCase("Ravi")	&& strPwd.equalsIgnoreCase("Cluster")) {
+			rd = req.getRequestDispatcher("./jsp/success.jsp");
+			rd.forward(req, res);
+		} else {
+			rd = req.getRequestDispatcher("./jsp/failure.jsp");
+			rd.forward(req, res);
+		}
+	}
 }
