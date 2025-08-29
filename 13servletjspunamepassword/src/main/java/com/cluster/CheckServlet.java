3176dfb0469ccc227359ef599ad382d37e387562
varun <varunkr9866@gmail.com>
2025-08-29 14:41:15 +0530

    STATRED THE PROJECT

---
+++ 13servletjspunamepassword/src/main/java/com/cluster/CheckServlet.java
@@ -0,0 +1,13 @@
+package com.cluster;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CheckServlet extends HttpServlet{
+	
+}
