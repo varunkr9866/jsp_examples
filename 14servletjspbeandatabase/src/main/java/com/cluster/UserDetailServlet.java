1936a0cb5e9c6dc10fc5cdacdce2b7984da209d5
varun <varunkr9866@gmail.com>
2025-08-30 15:53:47 +0530

    COMPLETED THE PROJECT

--- 14servletjspbeandatabase/src/main/java/com/cluster/UserDetailServlet.java
+++ 14servletjspbeandatabase/src/main/java/com/cluster/UserDetailServlet.java
@@ -9,6 +9,12 @@ import java.sql.SQLException;
 import com.cluster.to.PersonTO;
 import com.cluster.util.DBUtil;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class UserDetailServlet  extends HttpServlet{
 	Connection con;
 
