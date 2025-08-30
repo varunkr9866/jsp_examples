77f181d60db6f1c62029b745f804d50f9211d747
varun <varunkr9866@gmail.com>
2025-08-30 15:35:48 +0530

    COMPLETED CONNECTION CODE

--- 14servletjspbeandatabase/src/main/java/com/cluster/util/DBUtil.java
+++ 14servletjspbeandatabase/src/main/java/com/cluster/util/DBUtil.java
@@ -1,5 +1,13 @@
 package com.cluster.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBUtil {
-	
+	public static Connection getOracleConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/varunmysql", "root","Cluster");
+		return con;
+	}
 }
