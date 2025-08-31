c1866040173cca38457ca2702b40d53f5c239903
varun <varunkr9866@gmail.com>
2025-08-31 11:02:20 +0530

    COMPLETED TO OBJECT CODE

---
+++ 16servletjspusebean/src/main/java/com/cluster/to/PersonTo.java
@@ -0,0 +1,42 @@
+package com.cluster.to;
+
+import java.io.Serializable;
+
+public class PersonTo implements Serializable {
+	private String name;
+	private String password;
+	private String phone;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
