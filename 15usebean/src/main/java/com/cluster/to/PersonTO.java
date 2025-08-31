1ecb5c96b40b955fa1c59965ba0d031ec79ea16e
varun <varunkr9866@gmail.com>
2025-08-31 10:39:19 +0530

    COMPLETED TO OBJECT CODE

---
+++ 15usebean/src/main/java/com/cluster/to/PersonTO.java
@@ -0,0 +1,26 @@
+package com.cluster.to;
+
+public class PersonTO {
+	private String firstname;
+	private String lastname;
+	private String email;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
